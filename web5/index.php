8ea6a0f8e40bd7c019d554471659baa53a9be0c7
GevelaL <you@example.com>
2022-03-07 04:49:13 -0800

    finished web5

--- web5/index.php
+++ web5/index.php
@@ -1,7 +1,23 @@
 <?php
+session_start();
+if(isset($_SESSION['num_visits'])){
+    $_SESSION['num_visits'] = $_SESSION['num_visits']+1;
+} else {
+$_SESSION['num_visits'] = 1;
+}
+if(isset($_POST['d'])){
+   session_destroy();
+   header("Location:index.php");
+}
+
 
 ?>
 
 <h1>Hello</h1>
-<p>You have visited this page <?= $num_visits ?> times</p>
-<p><a href="?reset=true">Reset</a></p>
+<p>You have visited this page <?php echo $_SESSION['num_visits']; ?> times</p>
+<p><form method="POST"><button name="d">reset</button></form></p>
+<?php
+    if($_SESSION['num_visits'] == 10 ){
+        include 'congratulations.php';
+    }
+?>
