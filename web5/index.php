f8b24ee1f2d7fc1484c8bf7aa74489849f3f6be4
Ralph Kevin <kevinmacahipay7@gmail.com>
2022-03-08 15:43:20 -0700

    done

--- web5/index.php
+++ web5/index.php
@@ -1,7 +1,32 @@
 <?php
+session_start();
 
+ if(isset($_SESSION['visitCount'])){
+     if ($_SESSION['visitCount'] == 4){
+        echo "<h1>Wow!</h1> You sure come to this page a lot</>";
+        $_SESSION['visitCount']++;
+        
+
+     }elseif($_SESSION['visitCount'] == 9){
+        header("Location: congratulations.php");
+        echo "cool";
+      
+
+     }else 
+        $_SESSION['visitCount']++;
+
+
+    
+    
+ }
+ else
+    $_SESSION['visitCount'] = 1;
+
+        
 ?>
 
 <h1>Hello</h1>
-<p>You have visited this page <?= $num_visits ?> times</p>
+<p>You have visited this page <?= $_SESSION['visitCount'] ?> times</p>
 <p><a href="?reset=true">Reset</a></p>
+<h1></h1>
+
